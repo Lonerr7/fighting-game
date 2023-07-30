1fda57ab5bbe285c411583272c84dcf0eb94e45b
Lonerr7 <krabik2342@gmail.com>
2023-07-30 12:27:24 +0300

    2. Created player and enemy instances

--- script.js
+++ script.js
@@ -7,20 +7,29 @@ const CANVAS_HEIGHT = (canvas.height = 576);
 // Красим канвас в черный цвет
 ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
+const gravity = 0.2;
+
 class Sprite {
   constructor({ position, velocity }) {
     this.position = position;
     this.velocity = velocity;
+    this.height = 150;
   }
 
   draw() {
     ctx.fillStyle = 'red';
-    ctx.fillRect(this.position.x, this.position.y, 50, 150);
+    ctx.fillRect(this.position.x, this.position.y, 50, this.height);
   }
 
   update() {
     this.draw();
-    this.position.y += 10;
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
+    }
   }
 }
 
@@ -35,8 +44,6 @@ const player = new Sprite({
   },
 });
 
-player.draw();
-
 const enemy = new Sprite({
   position: {
     x: 400,
@@ -48,11 +55,14 @@ const enemy = new Sprite({
   },
 });
 
-enemy.draw();
-
 // Создаем анимационный луп (animation loop)
 const animate = () => {
   window.requestAnimationFrame(animate);
+  // Clearing canvas to get rid of paint-like effect when animating player and enemy
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  player.update();
+  enemy.update();
 };
 
 animate();
